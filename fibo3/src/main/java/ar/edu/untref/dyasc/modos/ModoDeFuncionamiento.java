20b76b34ea7d9e1c4b7a2cb1f6454481b55f755e
gabrielbosio <gabrielbosio95@gmail.com>
2018-09-04 15:56:14 -0300

    Reentrega de fibo1: archivos innecesarios eliminados

--- fibo3/src/main/java/ar/edu/untref/dyasc/modos/ModoDeFuncionamiento.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.untref.dyasc.modos;
-
-import ar.edu.untref.dyasc.opciones.OpcionNoValidaException;
-
-public interface ModoDeFuncionamiento {
-    
-    public String ejecutar(int[] sucesion, String nombreDeOpcion)
-            throws OpcionNoValidaException;
-}
--- fibo3/src/main/java/ar/edu/untref/dyasc/opciones/Direccion.java
+++
@@ -1,6 +0,0 @@
-package ar.edu.untref.dyasc.opciones;
-
-public enum Direccion {
-    DIRECTA,
-    INVERSA
-}
